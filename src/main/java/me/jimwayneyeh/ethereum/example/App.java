c01b386a70b67df8e16c530a47adb1ae5f603a0d
Wayne Yeh <jimwayne@gmail.com>
2018-09-05 14:33:49 +0800

    Move to test net.

--- src/main/java/me/jimwayneyeh/ethereum/example/App.java
+++ src/main/java/me/jimwayneyeh/ethereum/example/App.java
@@ -1,19 +1,29 @@
 package me.jimwayneyeh.ethereum.example;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.ethereum.config.SystemProperties;
 import org.ethereum.facade.Ethereum;
 import org.ethereum.facade.EthereumFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.typesafe.config.ConfigFactory;
+
 public class App {
   private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
   
-  public static void main(String[] args) {
-    System.out.println("Start application.");
+  public static void main(String[] args) throws Throwable {
+    SystemProperties properties = SystemProperties.getDefault();
     
-    SystemProperties.getDefault().setSyncEnabled(false);
-    SystemProperties.getDefault().setDiscoveryEnabled(true);
+    try (InputStream is = App.class.getResourceAsStream("/ropsten.conf")) {
+      String config = IOUtils.toString(is, StandardCharsets.UTF_8);
+      properties.overrideParams(ConfigFactory.parseString(config));
+    }
+    properties.setSyncEnabled(true);
+    properties.setDiscoveryEnabled(true);
     
     Ethereum eth = EthereumFactory.createEthereum();
     eth.startPeerDiscovery();
